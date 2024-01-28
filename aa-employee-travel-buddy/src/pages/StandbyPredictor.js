8f6edc0d3b111ba2577ab2e9223b28852d30fa66
gjtras <gjtraslavina@tamu.edu>
2024-01-28 10:36:11 -0600

    standby predictor is almost done

--- aa-employee-travel-buddy/src/pages/StandbyPredictor.js
+++ aa-employee-travel-buddy/src/pages/StandbyPredictor.js
@@ -1,9 +1,143 @@
 // Gabe
-
-import React from 'react';
+import './StandbyPredictor.css'
+import React, { useState } from 'react';
 
 function StandbyPredictor() {
-    return <div>This is Page 1</div>;
+
+    const [formData, setFormData] = useState({
+        date: '',
+        origin: '',
+        destination: '',
+        departureTime: '',
+        arrivalTime: '',
+    });
+
+
+    const [randomNumber, setRandomNumber] = useState(null);
+    
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: value,
+        }));
+    };
+
+    const generateRandomNumber = () => {
+        // Simple bias: more chances to get numbers closer to 0
+        const biasFactor = Math.random() * Math.random();
+        return Math.floor(biasFactor * 24);
+      };
+
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        const randomNumber = generateRandomNumber();
+        setRandomNumber(randomNumber);
+      };
+
+
+
+    return <div className='Main'>
+        <h1>Please fill out the below fields to of your desired flight to see what our analytics show will be available</h1>
+        <form onSubmit={handleSubmit}>
+        <div className='Form Background'>
+            
+            <div className='Form'>
+            
+            <label>Date</label>
+            <br/>
+            
+            <input type="date"></input>
+            <br/>
+            <label for="origin">Choose Airport of Origin:</label>
+            <br/>
+            <select id="origin" name="origin">
+                <option value="DFW">Dallas/Fort Worth</option>
+                <option value="JFK">New York City</option>
+                <option value="LAX">Los Angeles</option>
+                <option value="ORD">Chicago</option>
+                <option value="GSO">Gree nsboro</option>
+                <option value="ATL">Atlanta</option>
+                <option value="DEN">Denver</option>
+                <option value="CLT">Charlette</option>
+                <option value="ORD">Chicago</option>
+                <option value="PHX">Phoenix</option>
+                <option value="MCO">Orlando</option>
+                <option value="SEA">Seattle</option>
+                <option value="MIA">Miami</option>
+                <option value="IAH">Houston</option>
+                <option value="FLL">Fort Lauderdale</option>
+                <option value="BWI">Baltimore</option>
+                <option value="SFO">San Francisco</option>
+                <option value="MSP">Minneapolis</option>
+                <option value="DTW">Detroit</option>
+                <option value="BOS">Boston</option>
+                <option value="TPA">Tampa</option>
+                <option value="SLC">Salt Lake City</option>
+                <option value="SAN">San Diego</option>
+                <option value="PHL">Philadelphia</option>
+            </select>
+
+            <br/>
+            <label for="dest">Choose Airport Destination:</label>
+            <br/>
+            <select id="dest" name="dest">
+                <option value="DFW">Dallas/Fort Worth</option>
+                <option value="JFK">New York City</option>
+                <option value="LAX">Los Angeles</option>
+                <option value="ORD">Chicago</option>
+                <option value="GSO">Greensboro</option>
+                <option value="ATL">Atlanta</option>
+                <option value="DEN">Denver</option>
+                <option value="CLT">Charlette</option>
+                <option value="ORD">Chicago</option>
+                <option value="PHX">Phoenix</option>
+                <option value="MCO">Orlando</option>
+                <option value="SEA">Seattle</option>
+                <option value="MIA">Miami</option>
+                <option value="IAH">Houston</option>
+                <option value="FLL">Fort Lauderdale</option>
+                <option value="BWI">Baltimore</option>
+                <option value="SFO">San Francisco</option>
+                <option value="MSP">Minneapolis</option>
+                <option value="DTW">Detroit</option>
+                <option value="BOS">Boston</option>
+                <option value="TPA">Tampa</option>
+                <option value="SLC">Salt Lake City</option>
+                <option value="SAN">San Diego</option>
+                <option value="PHL">Philadelphia</option>
+            </select>
+
+            <br/>
+            <label>Time of Departure </label>
+            
+            <br/><input type="time"></input>
+            <br/>
+            <label>Time of Arrival</label>
+            
+            <br/><input type="time"></input>
+            <br/>
+            <button>See Projected Availability</button>
+            
+        </div>
+        
+        </div>
+        </form>
+        <br/>   
+        <br/>
+
+        {randomNumber !== null && (
+        <div>
+          
+          
+          <p>Random Number (0-23 with bias towards 0): {randomNumber}</p>
+        </div>
+      )}
+    
+
+    </div>;
 }
 
 export default StandbyPredictor;
